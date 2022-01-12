875db182b127b25f410ec2b594c86e888bae5b84
tprpic <tomislav.prpic8@gmail.com>
2022-01-12 20:18:03 +0100

    test changes

--- src/test/java/com/evilhomework/prpic/CommandHandlerTest.java
+++ src/test/java/com/evilhomework/prpic/CommandHandlerTest.java
@@ -3,7 +3,6 @@ package com.evilhomework.prpic;
 import com.evilhomework.prpic.api.BreedService;
 import com.evilhomework.prpic.api.DogService;
 import com.evilhomework.prpic.exception.ValidationException;
-import com.evilhomework.prpic.fakes.DogServiceFake;
 import com.evilhomework.prpic.reports.BreedReport;
 import com.evilhomework.prpic.reports.DogReport;
 import org.junit.Before;
@@ -14,8 +13,7 @@ import org.mockito.MockitoAnnotations;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -44,17 +42,35 @@ public class CommandHandlerTest {
 
     @Test
     public void addsDogSuccessfully() throws SQLException {
-        when(dogService.addDog(any(), any(), any())).thenReturn(true);
+        when(dogService.addDog(anyString(), anyInt(), anyInt())).thenReturn(true);
 
         commandHandler.getCommand("add dog someDogName 5 1");
 
-        verify(dogService).addDog(eq("someDogName"), eq(5), eq(1));
+        verify(dogService).addDog(eq("someDogName"),eq(5), eq(1));
+    }
+
+    @Test
+    public void addsBreedSuccessfully() throws SQLException{
+        when(breedService.addBreed(anyString())).thenReturn(true);
+
+        commandHandler.getCommand("add breed someBreedName");
+
+        verify(breedService).addBreed(eq("someBreedName"));
     }
 
     @Test(expected = ValidationException.class)
     public void verifyAddDogThrowsWhenNameTooShort() throws SQLException {
         commandHandler.getCommand("add dog a 5 1");
     }
+
+    @Test
+    public void  listDogsSuccessfully() throws SQLException{
+        commandHandler.getCommand("list dogs");
+        verify(dogService.findDogs());
+    }
+
+
+
 }
 
 //test za novi branch
